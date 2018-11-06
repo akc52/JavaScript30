dc1947dc6c2daa4e28efa6da1befc81e2f23f8f9
Akc52 <a_lys_sa@hotmail.com>
2018-11-06 11:51:02 -0500

    Playing with rgb array to create filters

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -12,9 +12,97 @@ function getVideo() {
             video.src = window.URL.createObjectURL(localMediaStream);
             video.play()
         })
-        .catch( err=> {
+        .catch(err => {
             console.log('oh no', err)
         })
 }
 
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+    console.log(width, height);
+    setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        // take pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // console.log(pixels);
+        // debugger;
+
+        // change
+        // pixels = redEffect(pixels);
+
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.1;
+
+        pixels = greenScreen(pixels);
+
+        // return
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    // take data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    // console.log(data)
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome" Photo">`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i+=4 ){
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; // r
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // g
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // b
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for(let i = 0; i < pixels.data.length; i+=4 ){
+        pixels.data[i - 150] = pixels.data[i + 0]; // r
+        pixels.data[i + 100] = pixels.data[i + 1]; // g
+        pixels.data[i + 150] = pixels.data[i + 2]; // b
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    // hold min/max 'green'
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+  
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // if between these values, take it out!
+        pixels.data[i + 3] = 0;
+      }
+    }
+  
+    return pixels;
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas)
